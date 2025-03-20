fc2299cd3a6db97754ebf8c8ff14f49ff62771c2
Jamil Lambert, PhD <Jamil.Lambert@proton.me>
2025-03-20 09:10:40 +0000

    Add extra fields and setters to Request

--- nostd-rpc/src/http.rs
+++ nostd-rpc/src/http.rs
@@ -1,4 +1,5 @@
 use alloc::string::String;
+use alloc::vec::Vec;
 use core::time::Duration;
 
 const DEFAULT_URL: &str = "http://localhost";
@@ -6,28 +7,67 @@ const DEFAULT_PORT: u16 = 8332; // the default RPC port for bitcoind.
 const DEFAULT_TIMEOUT_SECONDS: u64 = 15;
 
 #[derive(Clone, Debug)]
-pub struct MinreqHttpTransport {
+pub struct HttpRequest {
     /// URL of the RPC server.
     url: String,
+    /// HTTP method, e.g., "POST".
+    method: String,
+    /// HTTP headers.
+    headers: Vec<String>,
+    /// Body of the HTTP request.
+    body: String,
     /// timeout only supports second granularity.
     timeout: Duration,
     /// The value of the `Authorization` HTTP header, i.e., a base64 encoding of 'user:password'.
     basic_auth: Option<String>,
 }
 
-impl Default for MinreqHttpTransport {
+impl Default for HttpRequest {
     fn default() -> Self {
-        MinreqHttpTransport {
+        HttpRequest {
             url: append_port(DEFAULT_URL, DEFAULT_PORT),
+            method: String::from("POST"),
+            headers: Vec::new(),
+            body: String::new(),
             timeout: Duration::from_secs(DEFAULT_TIMEOUT_SECONDS),
             basic_auth: None,
         }
     }
 }
 
-impl MinreqHttpTransport {
+impl HttpRequest {
     /// Constructs a new [`MinreqHttpTransport`] with default parameters.
-    pub fn new() -> Self { MinreqHttpTransport::default() }
+    pub fn new() -> Self { HttpRequest::default() }
+
+    /// Sets the URL of the RPC server.
+    pub fn url(mut self, url: &str) -> Self {
+        self.url = append_port(url, DEFAULT_PORT);
+        self
+    }
+
+    /// Sets the HTTP method.
+    pub fn method(mut self, method: &str) -> Self {
+        self.method = String::from(method);
+        self
+    }
+
+    /// Adds an HTTP header.
+    pub fn header(mut self, header: &str) -> Self {
+        self.headers.push(String::from(header));
+        self
+    }
+
+    /// Sets the body of the HTTP request.
+    pub fn body(mut self, body: &str) -> Self {
+        self.body = String::from(body);
+        self
+    }
+
+    /// Sets the timeout for the HTTP request.
+    pub fn timeout(mut self, timeout: Duration) -> Self {
+        self.timeout = timeout;
+        self
+    }
 }
 
 fn append_port(url: &str, port: u16) -> String {
