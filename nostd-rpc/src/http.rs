63ff5728ba4a1629184db83a5720e06c2cb72559
Jamil Lambert, PhD <Jamil.Lambert@proton.me>
2025-04-03 20:32:50 +0100

    Big rewrite with std to try and get working

--- nostd-rpc/src/http.rs
+++ nostd-rpc/src/http.rs
@@ -2,11 +2,15 @@ use alloc::string::String;
 use alloc::vec::Vec;
 use alloc::vec;
 
+use std::io;
+use std::os::unix::io::AsRawFd;
+use log::trace;
+
 use smoltcp::iface::{Config, Interface, SocketSet};
 use smoltcp::socket::tcp;
 use smoltcp::time::{Duration, Instant};
 use smoltcp::wire::{EthernetAddress, IpAddress, Ipv4Address, IpCidr};
-use smoltcp::phy::{TunTapInterface, Medium};
+use smoltcp::phy::{wait as phy_wait, PcapMode, TunTapInterface, FaultInjector, Medium, Tracer, PcapWriter};
 
 const DEFAULT_URL: &str = "http://localhost";
 const DEFAULT_PORT: u16 = 8332; // the default RPC port for bitcoind.
@@ -100,30 +104,34 @@ impl HttpRequest {
 }
 
 pub fn send(
+    ethernet_mac: [u8; 6],
     ip: [u8; 4],
     port: u16,
     _payload: String,
-) -> String {
-    let tcp_rx_buffer = tcp::SocketBuffer::new(vec![0; 1024]);
-    let tcp_tx_buffer = tcp::SocketBuffer::new(vec![0; 1024]);
-    let tcp_socket = tcp::Socket::new(tcp_rx_buffer, tcp_tx_buffer);
-    let mut device = TunTapInterface::new("tap0", Medium::Ethernet).unwrap();
-    let mut config = Config::new(EthernetAddress([0x00, 0x15, 0x5d, 0xc7, 0xbf, 0x6d]).into()); //00:15:5d:c7:bf:6d
+) -> Result<String, &'static str> {
+    let tuntap = TunTapInterface::new("tap", Medium::Ethernet).map_err(|_| "Failed to create TUN/TAP interface")?;
+    let fd = tuntap.as_raw_fd();
+    let mut device = create_device(tuntap);
+    let mut config = Config::new(EthernetAddress(ethernet_mac).into());
     config.random_seed = 0; // Use a fixed seed for testing purposes.
 
     let mut iface = Interface::new(config, &mut device, Instant::now());
     iface.update_ip_addrs(|ip_addrs| {
         ip_addrs
             .push(IpCidr::new(IpAddress::v4(172, 28, 24, 156), 20)) // Local IP with subnet mask
-            .unwrap();
+            .map_err(|_| "Failed to update IP addresses").unwrap()
     });
     iface
         .routes_mut()
-        .add_default_ipv4_route(Ipv4Address::new(172, 28, 16, 1)) // Default gateway
-        .unwrap();
+        .add_default_ipv4_route(Ipv4Address::new(172, 28, 16, 0)) // Default gateway
+        .map_err(|_| "Failed to add default route")?;
 
+    let tcp_rx_buffer = tcp::SocketBuffer::new(vec![0; 1024]);
+    let tcp_tx_buffer = tcp::SocketBuffer::new(vec![0; 1024]);
+    let tcp_socket = tcp::Socket::new(tcp_rx_buffer, tcp_tx_buffer);
     let mut sockets = SocketSet::new(vec![]);
-    let tcp_handle = sockets.add(tcp_socket);  let ipv4 = Ipv4Address::new(ip[0], ip[1], ip[2], ip[3]);
+    let tcp_handle = sockets.add(tcp_socket);
+    let ipv4 = Ipv4Address::new(ip[0], ip[1], ip[2], ip[3]);
 
     enum State {
         Connect,
@@ -132,57 +140,87 @@ pub fn send(
     }
     let mut state = State::Connect;
 
-    let mut response = String::new();
+    let mut response = String::from("Response: ");
 
-    for _ in 0..100 {
+    loop {
         let timestamp = Instant::now();
-        iface.poll(timestamp, &mut device, &mut sockets);
+        let _ = iface.poll(timestamp, &mut device, &mut sockets);
 
         let socket = sockets.get_mut::<tcp::Socket>(tcp_handle);
         let cx = iface.context();
 
         state = match state {
             State::Connect if !socket.is_active() => {
-                let _ = socket
-                    .connect(cx, (ipv4, 80), port);
+                socket
+                    .connect(cx, (ipv4, 80), port)
+                    .map_err(|_| "Failed to connect")?;
+                response.push_str("Connected to server.\n");
                 State::Request
             }
             State::Request if socket.may_send() => {
-                let http_get = "GET www.example.org HTTP/1.1\r\n";
-                socket.send_slice(http_get.as_ref()).expect("cannot send");
-                let http_host = "Host: http://www.example.org/\r\n";
-                socket.send_slice(http_host.as_ref()).expect("cannot send");
+                response.push_str("Request.\n");
+                let http_get = "GET /index.html HTTP/1.1\r\n";
+                let send_rsp1 = socket.send_slice(http_get.as_ref()).map_err(|_| "Failed to send GET request")?;
+                response.push_str("send rsp 1: ");
+                response.push_str(&u16_to_string(send_rsp1 as u16));
+                response.push('\n');
+                let http_host = "Host: www.example.org\r\n";
+                socket.send_slice(http_host.as_ref()).map_err(|_| "Failed to send Host header")?;
                 socket
                     .send_slice(b"Connection: close\r\n")
-                    .expect("cannot send");
-                socket.send_slice(b"\r\n").expect("cannot send");
+                    .map_err(|_| "Failed to send Connection header")?;
+                socket.send_slice(b"\r\n").map_err(|_| "Failed to send final CRLF")?;
                 State::Response
             }
             State::Response if socket.can_recv() => {
                 socket
                     .recv(|data| {
-                        // Append the received data to the response buffer
-                        response.push_str(alloc::str::from_utf8(data).expect("Invalid UTF-8 sequence"));
+                        response.push_str(alloc::str::from_utf8(data).unwrap_or("(invalid utf8)"));
                         (data.len(), ())
                     })
-                    .unwrap();
+                    .map_err(|_| "Failed to receive data")?;
                 State::Response
             }
             State::Response if !socket.may_recv() => break,
             _ => state,
         };
 
-        let start_time = Instant::now();
-        let mut waiting = 0;
-        // wait for 0.1 seconds
-        while waiting < 100 {
-            let current_time = Instant::now();
-            let duration = current_time - start_time;
-            waiting = duration.total_millis();
-        }
+        phy_wait(fd, iface.poll_delay(timestamp, &sockets)).expect("wait error");
     }
 
-    response
+    Ok(response)
+}
+
+fn create_device(tuntap: TunTapInterface) -> FaultInjector<Tracer<PcapWriter<TunTapInterface, Box<dyn io::Write>>>> {
+    let drop_chance = 0;
+    let corrupt_chance = 0;
+    let size_limit = 0;
+    let tx_rate_limit = 0;
+    let rx_rate_limit = 0;
+    let shaping_interval = 0;
+
+    let pcap_writer: Box<dyn io::Write> = Box::new(io::sink());
+
+    let seed = 0;
+
+    let device = PcapWriter::new(
+        tuntap,
+        pcap_writer,
+        PcapMode::Both,
+    );
+
+    let device = Tracer::new(device, |_timestamp, _printer| {
+        trace!("{}", _printer);
+    });
+
+    let mut device = FaultInjector::new(device, seed);
+    device.set_drop_chance(drop_chance);
+    device.set_corrupt_chance(corrupt_chance);
+    device.set_max_packet_size(size_limit);
+    device.set_max_tx_rate(tx_rate_limit);
+    device.set_max_rx_rate(rx_rate_limit);
+    device.set_bucket_interval(Duration::from_millis(shaping_interval));
+    device
 }
 
 fn append_port(url: &str, port: u16) -> String {
@@ -227,8 +265,9 @@ mod tests {
         let ip = [81, 130, 109, 40];
         let port = 1234;
         let payload = String::from("Test payload");
+        let ethernet_mac = [0x00, 0x15, 0x5d, 0xc7, 0xbf, 0x6d];
 
-        let result = send(ip, port, payload);
-        assert_eq!(result, "expected result");
+        let result = send(ethernet_mac, ip, port, payload);
+        assert_eq!(result.unwrap(), "expected result");
     }
 }
--- nostd-rpc/src/lib.rs
+++ nostd-rpc/src/lib.rs
@@ -1,4 +1,3 @@
-#![no_std]
 #![allow(dead_code)]  // TODO: Remove this line when not needed anymore.
 extern crate alloc;
 
